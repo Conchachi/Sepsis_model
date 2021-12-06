acd80ec96c4b3a24df91641b96d2c7e09fea42c7
dyang44 <70005153+dyang44@users.noreply.github.com>
2021-12-06 12:36:49 -0500

    updated classify_validating for dynamic dataset

--- classify_validating.m
+++ classify_validating.m
@@ -1,7 +1,29 @@
 %This code creates and tests a model of septic risk
+
+%% Static dataset
 clear
+glm_part1
+clearvars -except B X Y % save coefficients
+
+%bring in testing data
+load('static_data_validating.mat');
+load('dynamic_data_validating.mat');
+
+% static validating
+X = static_val(:,5:7);
+Phat_val_static = 1./(1+exp(-[ones(size(X,1),1) X]*B));
+
+Y_test = static_val(:,2);
+[threshold] = test_performance(Phat_val_static, Y_test);
+
+Y_bestguess(Phat_val_static>threshold,1) = 1;
+Y_bestguess(Phat_val_static<threshold,1) = 0;
+
+PercentCorrect_static = (1 - sum(abs(Y_test-Y_bestguess))/length(Y_test))*100
+
+%% Dynamic dataset
 glm_part2
-clearvars -except B % save coefficients
+clearvars -except B static_train dynamic_train
 
 %bring in testing data
 load('static_data_validating.mat');
@@ -10,26 +32,40 @@ load('dynamic_data_validating.mat');
 %extract variables for data structures
 Y_val = nan(length(dynamic_val(:,1)),1);
 X_val = nan(length(dynamic_val(:,1)),11);
-X_val(:,6:11) = dynamic_val(:,3:8);
+X(:,6:11) = dynamic_train(:,3:8); %controls which dynamic data is to be used
 IDs = dynamic_val(:,1);%septic patient ID for each waveform time point
 ID_uni = static_val(:,1);%patient's ID numbers
 
 %create covariate matrix including both demographic info and waveform data
 for i = 1:length(ID_uni)%for septic data
     ind = find(IDs==ID_uni(i));
-    X_val(ind,1:5) = repmat(static_train(i,3:7),length(ind),1);
-    display(num2str(i));
-    Y_val(ind) = repmat(static_train(i,2),length(ind),1);
+    
+    %%%%
+    X(ind,1:5) = repmat(static_train(i,[3,4,5,6,7]),length(ind),1); %combines static with dynamic
+    %X(ind,1:2) = repmat(static_train(i,[5,7]),length(ind),1);  %only parameters that have consistently been p-values of 0
+    %%%%
+    %display(num2str(i));
+    Y_val(ind) = repmat(static_train(i,2),length(ind),1); 
 end
 
 %%%%%%%%COMPUTE TEST MODEL USING B FROM TRAINING MODEL
-Phat_val = 1./(1+exp(-[ones(size(X_val,1),1) X_val]*B));
+Phat_val = 1./(1+exp(-[ones(size(X,1),1) X]*B));
 
 %Implement your decision rule for each patient here.
 
-%YOU ADD YOUR CLASSIFIER HERE!!!! 
-% Y_test_bestguess = ????????????????
+Phat_decision = nan(size(ID_uni));
+Y_decision = nan(size(ID_uni));
+for i = 1:length(ID_uni)%for septic data
+    ind = find(IDs==ID_uni(i));
+    Phat_decision(i) = max(Phat_val(ind));
+    Y_decision(i) = static_train(i,2);
+end 
 
-% PercentCorrect = (1 - sum(abs(Y_test-Y_test_bestguess))/length(Y_test))*100
+[threshold] = test_performance(Phat_decision, Y_decision);
+
+Y_bestguess(Phat_decision>threshold,1) = 1;
+Y_bestguess(Phat_decision<threshold,1) = 0;
+
+PercentCorrect_dynamic = (1 - sum(abs(Y_decision-Y_bestguess))/length(Y_decision))*100
 
 
